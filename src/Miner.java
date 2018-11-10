5a05c858e7cd8860f845cd1c41f21bc8364f35e5
Razvan Radoi <a-rarad@microsoft.com>
2018-11-10 13:17:42 +0200

    add constructor for miner

--- src/Miner.java
+++ src/Miner.java
@@ -1,5 +1,6 @@
 import java.util.Set;
-
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
 /**
  * Class for a miner.
  */
@@ -7,18 +8,30 @@ public class Miner extends Thread {
 	/**
 	 * Creates a {@code Miner} object.
 	 * 
-	 * @param hashCount
+	 * @param newHashCount
 	 *            number of times that a miner repeats the hash operation when
 	 *            solving a puzzle.
-	 * @param solved
+	 * @param newSolved
 	 *            set containing the IDs of the solved rooms
-	 * @param channel
+	 * @param newChannel
 	 *            communication channel between the miners and the wizards
 	 */
-	public Miner(Integer hashCount, Set<Integer> solved, CommunicationChannel channel) {
+
+	public static AtomicInteger hashCount;
+	public static ConcurrentHashMap<Integer, Object> solved;
+	public static CommunicationChannel channel;
+
+	public Miner(Integer newHashCount, Set<Integer> newSolved, CommunicationChannel newChannel) {
+		channel = newChannel;
+		hashCount = new AtomicInteger((int)newHashCount);
+
+		for (Integer value : newSolved) {
+			solved.putIfAbsent(value, new Object());
+		}
 	}
 
 	@Override
 	public void run() {
+		// TODO IMPLEMENT ME 
 	}
 }
