fd5c60244b4d6f0ab2f290d3af0a8b18bdc70ba1
razvan <razvanradoi97@gmail.com>
2018-11-17 15:18:38 +0200

    good miner model (1 miner 1 wizard works)

--- src/Miner.java
+++ src/Miner.java
@@ -12,13 +12,14 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
+import java.util.concurrent.Semaphore;
 /**
  * Class for a miner.
  */
 public class Miner extends Thread {
 	/**
 	 * Creates a {@code Miner} object.
-	 * 
+	 *
 	 * @param newHashCount
 	 *            number of times that a miner repeats the hash operation when
 	 *            solving a puzzle.
@@ -32,41 +33,7 @@ public class Miner extends Thread {
 	public static ConcurrentHashMap<Integer, Object> solved = new ConcurrentHashMap<>();
 	public static CommunicationChannel channel = new CommunicationChannel();
 
-	public Miner(Integer newHashCount, Set<Integer> newSolved, CommunicationChannel newChannel) {
-		channel = newChannel;
-		hashCount = new AtomicInteger((int)newHashCount);
-
-		for (Integer value : newSolved) {
-			solved.putIfAbsent(value, new Object());
-		}
-	}
-
-	@Override
-	public void run() {
-		// get a message from the wizard telling the miner what to mine
-		// as long as it is not the exit message
-		Message messageFromWizard = channel.getMessageWizardChannel();
-		
-		while (messageFromWizard.getData() != "EXIT") {
-			// get data inside the message
-			String dataFromWizard = messageFromWizard.getData();
-			Integer parentRoom = messageFromWizard.getParentRoom();
-			Integer currentRoom = messageFromWizard.getCurrentRoom();
-			
-			// mine the data
-			String dataForWizard = encryptMultipleTimes(dataFromWizard, hashCount.get());
-			// build new message for the wizard
-			Message messageToWizard = new Message(
-										parentRoom,
-										currentRoom,
-										dataForWizard
-									);
-			// mark room as solved (TODO: figure out why)
-			solved.putIfAbsent(currentRoom, new Object());
-			// at the end, get a new message from the wizard
-			messageFromWizard = channel.getMessageWizardChannel();
-		}
-	}
+	private static Semaphore minerSemaphore = new Semaphore(1);
 
 	// method from solver
 	private static String encryptMultipleTimes(String input, Integer count) {
@@ -82,7 +49,7 @@ public class Miner extends Thread {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             byte[] messageDigest = md.digest(input.getBytes(StandardCharsets.UTF_8));
-            
+
             // convert to string
             StringBuffer hexString = new StringBuffer();
             for (int i = 0; i < messageDigest.length; i++) {
@@ -91,9 +58,68 @@ public class Miner extends Thread {
                 hexString.append(hex);
             }
             return hexString.toString();
-    
+
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
+
+	public Miner(Integer newHashCount, Set<Integer> newSolved, CommunicationChannel newChannel) {
+		channel = newChannel;
+		hashCount = new AtomicInteger((int)newHashCount);
+
+		for (Integer value : newSolved) {
+			solved.putIfAbsent(value, new Object());
+		}
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				minerSemaphore.acquire();
+			} catch (Exception ex) {
+				System.out.println(ex);
+			}
+			Message messageFromWizards = channel.getMessageWizardChannel();
+
+			// EXIT case -> thread (miner) should die
+			if (messageFromWizards.getData() == Wizard.EXIT) {
+				System.out.println("READ EXIT");
+				minerSemaphore.release();
+				return;
+			}
+
+			// END case -> 2nd message should not be read
+			if (messageFromWizards.getData() == Wizard.END) {
+				System.out.println("READ END");
+				minerSemaphore.release();
+				continue;
+			}
+
+			// parent room case
+			int parentRoomNo = messageFromWizards.getCurrentRoom();
+			System.out.println("READ PARENT");
+			// read child room after reading parent room
+			messageFromWizards = channel.getMessageWizardChannel();
+			System.out.println("READ ROOM");
+
+			int childRoomNo = messageFromWizards.getCurrentRoom();
+			String crypticMessage = messageFromWizards.getData();
+
+			String decryptedMessage = encryptMultipleTimes(crypticMessage,
+			 							hashCount.get());
+
+			// build answer for the mighty wizards
+			Message messageToWizards = new Message(parentRoomNo, childRoomNo,
+										decryptedMessage);
+
+			// tell Dumbledore
+			channel.putMessageMinerChannel(messageToWizards);
+
+			// release semaphore asap
+			minerSemaphore.release();
+		}
+	}
+
 }
