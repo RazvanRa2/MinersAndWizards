35cb54d33d1dcde214c71a19dd44de5061bca32f
razvan <razvanradoi97@gmail.com>
2018-11-17 18:30:54 +0200

    Mem messages from wizard attempt

--- src/CommunicationChannel.java
+++ src/CommunicationChannel.java
@@ -1,4 +1,7 @@
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.*;
+import java.util.LinkedList;
+import java.lang.*;
 
 /**
  * Class that implements the channel used by wizards and miners to communicate.
@@ -11,9 +14,13 @@ public class CommunicationChannel {
 	ConcurrentLinkedQueue<Message> minerChannel;
 	ConcurrentLinkedQueue<Message> wizardChannel;
 
+	ConcurrentHashMap<Integer, LinkedList<Message>> wizardMessages;
+
 	public CommunicationChannel() {
 		minerChannel = new ConcurrentLinkedQueue<Message>();
 		wizardChannel = new ConcurrentLinkedQueue<Message>();
+
+		wizardMessages =  new ConcurrentHashMap<Integer, LinkedList<Message>>();
 	}
 
 	/**
@@ -24,7 +31,7 @@ public class CommunicationChannel {
 	 *            message to be put on the channel
 	 */
 	public void putMessageMinerChannel(Message message) {
-		System.out.println("minner channel: " + message.getData() + "\t" + message.getCurrentRoom() + "\t" + message.getParentRoom());
+		System.out.println("minner channel: " + message.getData());
 		minerChannel.add(message);
 	}
 
@@ -52,7 +59,41 @@ public class CommunicationChannel {
 	 */
 	public void putMessageWizardChannel(Message message) {
 		System.out.println("wizard channel: " + message.getData());
-		wizardChannel.add(message);
+
+		// if it's an exit message, we don't care. just put it there
+		if (message.getData() == Wizard.EXIT) {
+
+			wizardChannel.add(message);
+			return;
+		}
+
+		// get the wizard's id
+		int wizardId = (int) Thread.currentThread().getId();
+
+		// if it's an end message, add all the messages for this end message
+		if (message.getData() == Wizard.END) {
+			LinkedList<Message> wizardList = wizardMessages.get(wizardId);
+			wizardList.add(message);
+
+			synchronized (wizardChannel) {
+				wizardChannel.addAll(wizardList);
+			}
+
+			return;
+		}
+
+		// if it's a room message, add it to the stored messages
+		if (wizardMessages.containsKey(wizardId)) {
+			LinkedList<Message> wizardOldList = wizardMessages.get(wizardId);
+			@SuppressWarnings("unchecked")  // damn java warnings
+			LinkedList<Message> wizardNewList = (LinkedList<Message>) wizardOldList.clone();
+			wizardNewList.add(message);
+			wizardMessages.replace(wizardId, wizardOldList, wizardNewList);
+		} else {
+			LinkedList<Message> newWizardList = new LinkedList<Message>();
+			newWizardList.add(message);
+			wizardMessages.put(wizardId, newWizardList);
+		}
 	}
 
 	/**
